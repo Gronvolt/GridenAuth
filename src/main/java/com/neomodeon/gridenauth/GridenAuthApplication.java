78aa7bddbbe521c02746b6896355c52de7a13ee4
Gronvolt <oleg.gronvolt@gmail.com>
2025-06-06 00:02:26 +0200

    Initial commit

---
+++ src/main/java/com/neomodeon/gridenauth/GridenAuthApplication.java
@@ -0,0 +1,13 @@
+package com.neomodeon.gridenauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridenAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridenAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neomodeon/gridenauth/GridenAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neomodeon.gridenauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridenAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
