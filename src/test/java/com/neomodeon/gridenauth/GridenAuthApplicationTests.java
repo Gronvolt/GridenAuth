7c8667671b151a3e7f3a3ef8c5d35f46fae10bc1
Gronvolt <oleg.gronvolt@gmail.com>
2025-06-06 14:54:52 +0200

    Добавлена БД, docker-compose, liquibase

--- src/test/java/com/neomodeon/gridenauth/GridenAuthApplicationTests.java
+++ src/test/java/com/neomodeon/gridenauth/GridenAuthApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GridenAuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
